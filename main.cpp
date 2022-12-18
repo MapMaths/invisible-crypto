b57aafe483e0a44ca35d04666ea9e0372717ffb0
MapMaths <mapmaths@outlook.com>
2022-12-18 09:18:05 +0800

    🤔 Using wide-char for Windows
    Using "wcout" and "L"String grammer to enable my computer (which use Win64 and GCC) to display the strange unicode. Might add more conditions

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
-// encode the input text via inserting a special character '\u200b' at a certain position
-
 #include <iostream>
+#if defined(__GNUC__) && defined(_WIN64)
+    #include <fcntl.h>
+    #define setmode _setmode(_fileno(stdout),_O_U16TEXT)
+#else
+    #define setmode
+#endif
 
 using namespace std;
 
 int main() {
+    setmode;
     char c;
     int count = 0;
     cin.get(c);
     while(c != '\n') {
         for(int i = 0; i < (int)c - 47; i++) {
-            cout << count % 10;
+            wcout << count % 10;
             count++;
         }
-        cout << "​";
+        wcout << L"​";
         cin.get(c);
     }
-    cout << count % 10 << endl;
+    wcout << count % 10 << endl;
     return 0;
 }
